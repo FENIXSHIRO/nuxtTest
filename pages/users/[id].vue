0c0df45a1ee790a41f4b78c4fb0a1307a8d4551f
Ivan Teteev <employee6523@export-base.ru>
2024-11-25 11:32:32 +0300

    refactor: данные получаются на сервере до рендеринга

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
 import { useMySteamUserStoreStore } from '@/ts/stores/steamUserStore'
-import { useRoute } from 'vue-router'
+import { useRoute, useAsyncData } from 'nuxt/app'
 
 const route = useRoute()
 const store = useMySteamUserStoreStore()
 
-// Загружаем данные пользователя при загрузке страницы
-store.fetchUserData(route.params.id.toString())
+const { data: userData } = await useAsyncData('user', async () => {
+  await store.fetchUserData(route.params.id.toString())
+  const user = {
+    userID: store.userID,
+    userName: store.userName,
+    avatarURL: store.avatarURL
+  }
+  return user
+})
 </script>
 
 <template>
-  <div>
+  <div class="mx-auto p-5 rounded-md border border-gray-200 w-fit">
     <p>Current route: {{ route.path }}</p>
-    <div v-if="store.userName">
-      <h1>{{ store.userName }}</h1>
-      <img :src="store.avatarURL" alt="Avatar" />
-      <p>Steam ID: {{ store.userID }}</p>
+    <div v-if="userData">
+      <h1>{{ userData.userName }}</h1>
+      <img :src="userData.avatarURL" alt="Avatar" />
+      <p>Steam ID: {{ userData.userID }}</p>
     </div>
-    <div v-else>
-    <p>Загрузка...</p>
-  </div>
   </div>
 </template>
-  
-<style>
-
-</style>
